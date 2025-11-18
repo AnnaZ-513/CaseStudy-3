389e517a9d1622902cbf6a1af00782eec66d75dc
AnnaZ-513 <anna.zhang@wustl.edu>
2025-11-17 20:54:10 -0600

    renamed a file

--- cs3_part2.m
+++ cs3_part2.m
@@ -1,3 +1,5 @@
+clear; clc; close all;
+
 %function [img,x,y] = rays2img(rays_x,rays_y,width,Npixels)
-lF = load('lightField.mat');
-ray = rays2img(lF(1,:),lF(3,:),0.005,200)
+load('lightField.mat');
+ray = rays2_img(rays(1,:),rays(3,:),0.005,200)
