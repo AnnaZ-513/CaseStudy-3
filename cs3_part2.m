9ad8120499569164f9a6ae474b7e284b011d17d7
AnnaZ-513 <anna.zhang@wustl.edu>
2025-12-03 20:46:36 -0600

    Created functions to see all images

--- cs3_part2.m
+++ cs3_part2.m
@@ -20,7 +20,7 @@ imshow(rays2img(rays(1,:),rays(3,:),0.01,700));
 N = length(rays); 
 angles = linspace(-pi/20, pi/20, N);
 
-d = 0.00001;   
+d = 0;   
 
 original = rays;
 
@@ -89,11 +89,9 @@ Md2 = [1 d2 0 0; 0 1 0 0; 0 0 1 d2; 0 0 0 1];
 
 Mf = [1 0 0 0; -1/f 1 0 0; 0 0 1 0; 0 0 -1/f 1];
 
-figure; 
 %rays_in_obj2_new = rays_out_obj2(:, abs(rays_out_obj2(1,:)) <= r_lens); 
-%[-0.01,0.01] gives sinopoli. THIS SIZE MAKES SENSE since the size of the
-%lens r that made it so we could ONLY see bruno was when it was 0.02, so
-%it had to be in the hundredths place so 0.01 was the natural step
+%[-0.02,0.02] gives sinopoli. THIS SIZE MAKES SENSE since the size of the
+%lens r that made it so we could ONLY see bruno was when it was 0.02
 %[-1,-0.02] give the robot
 %[0.02,1] gives washu sign
 
@@ -102,33 +100,40 @@ hits2 = (abs(original(1,:)) <= r);
 
 rays_after = Mf * original(:, hits2);
 rays_final = Md2 * rays_after; 
-
-num = rays_final;
-lower = 0.02;
-upper = 1;
-
-%this for loop counts how many rays are between a specific angle. We then
-%create an array that's precisely that size of valid angles
-for r = 1:length(rays_final)
-    if (rays_final(2, r) >= lower) && (rays_final(2, r) <=upper)
-        num(2,r) = 1;
-    else
-        num(2,r) = 0;
-    end
-end
-
-
-l=sum(num(2,:)); %calculating the sum of angles in the range
-final_angles = zeros(4,l); %(currently) empty array of filtered angles 
-
-count = 0;
-for r = 1:length(rays_final)
-    if (rays_final(2, r) >= lower) && (rays_final(2, r) <= upper)
-        count = count+1;
-        final_angles(:,count) = rays_final(:,r);
-    end
-end
-
-
-imshow(rays2img(final_angles(1,:),final_angles(3,:),0.003,375));
 %Filter out angles using trial and error for each image
+
+function [final_rays] = createImage(lower,upper, rays)
+    num = rays;
+    %this for loop counts how many rays are between a specific angle. We then
+    %create an array that's precisely that size of valid angles
+    for r = 1:length(rays)
+        if (rays(2, r) >= lower) && (rays(2, r) <=upper)
+            num(2,r) = 1;
+        else
+            num(2,r) = 0;
+        end
+    end
+
+    l=sum(num(2,:)); %calculating the sum of angles in the range
+    final_rays = zeros(4,l); %(currently) empty array of filtered angles 
+    
+    count = 0;
+    for r = 1:length(rays)
+        if (rays(2, r) >= lower) && (rays(2, r) <= upper)
+            count = count+1;
+            final_rays(:,count) = rays(:,r);
+        end
+    end
+end
+
+sinopoli = createImage(-0.02,0.02,rays_final);
+robot = createImage(-1,-0.02,rays_final);
+washU = createImage(0.02,1,rays_final);
+figure;
+imshow(rays2img(sinopoli(1,:),sinopoli(3,:),0.003,375));
+figure;
+imshow(rays2img(robot(1,:),robot(3,:),0.003,375));
+figure;
+imshow(rays2img(washU(1,:),washU(3,:),0.003,375));
+
+
